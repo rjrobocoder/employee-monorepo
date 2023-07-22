c9c3289a1c745e9ffdd7460b161e30e16ccb5063
rjbiswas <rajesh.biswas@nettantra.net>
2023-07-23 03:09:57 +0530

    Added EditEmployee custom modal component

--- frontend/src/app/components/Modals/EditEmployee.js
+++ frontend/src/app/components/Modals/EditEmployee.js
@@ -1,11 +1,68 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const EditEmployee = () => {
+const EditEmployee = ({ employeeId, onSubmit, closeModal }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [status, setStatus] = useState("");
+
+  const fetchEmployeeById = async (id) => {
+    try {
+      const response = await axios.get(
+        `http://127.0.0.1:8000/api/employees/${id}`
+      );
+      const { name, email, status } = response.data;
+      setName(name);
+      setEmail(email);
+      setStatus(status);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEmployeeById(employeeId);
+  }, [employeeId]);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "status") {
+      setStatus(value);
+    }
+  };
+
+  const handleUpdate = async () => {
+    try {
+      const updatedEmployee = {
+        name,
+        email,
+        status,
+      };
+
+      const response = await axios.put(
+        `http://127.0.0.1:8000/api/employees/${employeeId}`,
+        updatedEmployee
+      );
+
+      console.log("Update successful:", response.data);
+      closeModal();
+    } catch (error) {
+      console.log("Update failed:", error);
+    }
+    onSubmit();
+  };
+
   return (
     <div className="bg-[#1a1c1d96] h-screen w-full absolute top-0 z-[22] flex items-center justify-center">
       <div className="relative w-[399px] bg-white rounded-[7px] px-[64px] py-[34px]">
         <div className="absolute top-[27px] right-[30px]" title="close">
-          <button type="button">
+          <button type="button" onClick={closeModal}>
             <Image
               src="/images/close-icon-dark.svg"
               height={17}
@@ -32,6 +89,9 @@ const EditEmployee = () => {
                     type="text"
                     className="input-box-shadow border border-[#D7D7D7] rounded-[5px] h-[38px]"
                     id="name"
+                    name="name"
+                    value={name}
+                    onChange={handleInputChange}
                     required
                   />
                 </div>
@@ -48,6 +108,9 @@ const EditEmployee = () => {
                     type="email"
                     className="input-box-shadow border border-[#D7D7D7] rounded-[5px] h-[38px]"
                     id="email"
+                    name="email"
+                    value={email}
+                    onChange={handleInputChange}
                     required
                   />
                 </div>
@@ -63,6 +126,9 @@ const EditEmployee = () => {
                   <select
                     id="status"
                     className="input-box-shadow border border-[#D7D7D7] rounded-[5px] h-[38px]"
+                    name="status"
+                    value={status}
+                    onChange={handleInputChange}
                     required
                   >
                     <option value="">Select status</option>
@@ -74,11 +140,18 @@ const EditEmployee = () => {
             </ul>
           </div>
           <div className="mt-[50px] flex gap-4">
-            <button className="text-white text-[16px] font-semibold leading-[24px] bg-[#4D3AC1] w-full rounded-[4px] py-[7px] uppercase">
+            <button
+              type="button"
+              onClick={closeModal}
+              className="text-white text-[16px] font-semibold leading-[24px] bg-[#4D3AC1] w-full rounded-[4px] py-[7px] uppercase"
+            >
               Cancel
             </button>
-            <button className="text-white text-[16px] font-semibold leading-[24px] bg-[#4D3AC1] w-full rounded-[4px] py-[7px] uppercase">
-              Save
+            <button
+              className="text-white text-[16px] font-semibold leading-[24px] bg-[#4D3AC1] w-full rounded-[4px] py-[7px] uppercase"
+              onClick={handleUpdate}
+            >
+              Update
             </button>
           </div>
         </div>
